077d9a00a985d2aa2d29024decb35f45b7c93f0c
abhi15999 <abhishekgupta1599@gmail.com>
2021-10-03 12:02:00 +0530

    header-component | menu-divider

---
+++ src/component/Header.tsx
@@ -0,0 +1,32 @@
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+const Header = () => {
+  return (
+    <>
+      <div>
+        <h2
+          style={{ color: 'var(--primaryFont)' }}
+        >
+          Header
+        </h2>
+      </div>
+      <div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}
+        >
+          <h3
+            style={{ color: 'var(--secondaryFont)' }}
+          >
+            Filter
+          </h3>
+          <KeyboardArrowDownIcon style={{ fill: 'var(--secondaryFont)' }} />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
